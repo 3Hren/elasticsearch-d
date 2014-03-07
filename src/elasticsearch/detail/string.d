6cba0a567d29fd780a2f4b86aba564ee478cdd35
Evgeny Safronov <division494@gmail.com>
2014-03-07 14:55:52 +0400

    [Code style] Decomposition done.

---
+++ src/elasticsearch/detail/string.d
@@ -0,0 +1,37 @@
+module elasticsearch.detail.string;
+
+import std.regex;
+import std.string;
+
+string underscored(in string s) nothrow {
+    enum PrepareRE = ctRegex!(`::`);
+    enum FirstCapRX = ctRegex!(`([A-Z]+)([A-Z][a-z])`);
+    enum AllCapRX = ctRegex!(`([a-z\d])([A-Z])`);
+
+    try {
+        return replaceAll(replaceAll(replaceAll(s, PrepareRE, "/"), FirstCapRX, "$1_$2"), AllCapRX, "$1_$2").tr("-", "_").toLower();    
+    } catch (Exception) {
+        return s;
+    }
+}
+
+unittest {
+    assert("" == "".underscored);
+    assert("camel" == "camel".underscored);
+    assert("camel" == "Camel".underscored);
+    assert("camel_case" == "camelCase".underscored);
+    assert("camel_case" == "CamelCase".underscored);
+
+    assert("camel42" == "camel42".underscored);
+    assert("camel42" == "Camel42".underscored);
+    assert("camel42_case" == "camel42Case".underscored);
+    assert("camel42_case" == "Camel42Case".underscored);
+
+    assert("html" == "HTML".underscored);
+    assert("html5" == "HTML5".underscored);
+    assert("html_editor" == "HTMLEditor".underscored);
+    assert("html5_editor" == "HTML5Editor".underscored);
+    assert("editor_toc" == "editorTOC".underscored);
+    assert("editor_toc" == "EditorTOC".underscored);
+    assert("editor42_toc" == "Editor42TOC".underscored);    
+}
--- src/elasticsearch/domain/request/cluster/node/info.d
+++ src/elasticsearch/domain/request/cluster/node/info.d
@@ -2,46 +2,11 @@ module elasticsearch.domain.request.cluster.node.info;
 
 import std.array;
 import std.conv;
-import std.regex;
 import std.stdio;
-import std.string;
 import std.traits;
-import std.uni;
 
 import elasticsearch.domain.request.method;
-
-string underscored(in string s) nothrow {
-    enum PrepareRE = ctRegex!(`::`);
-    enum FirstCapRX = ctRegex!(`([A-Z]+)([A-Z][a-z])`);
-    enum AllCapRX = ctRegex!(`([a-z\d])([A-Z])`);
-
-    try {
-        return replaceAll(replaceAll(replaceAll(s, PrepareRE, "/"), FirstCapRX, "$1_$2"), AllCapRX, "$1_$2").tr("-", "_").toLower();    
-    } catch (Exception) {
-        return s;
-    }
-}
-
-unittest {
-    assert("" == "".underscored);
-    assert("camel" == "camel".underscored);
-    assert("camel" == "Camel".underscored);
-    assert("camel_case" == "camelCase".underscored);
-    assert("camel_case" == "CamelCase".underscored);
-
-    assert("camel42" == "camel42".underscored);
-    assert("camel42" == "Camel42".underscored);
-    assert("camel42_case" == "camel42Case".underscored);
-    assert("camel42_case" == "Camel42Case".underscored);
-
-    assert("html" == "HTML".underscored);
-    assert("html5" == "HTML5".underscored);
-    assert("html_editor" == "HTMLEditor".underscored);
-    assert("html5_editor" == "HTML5Editor".underscored);
-    assert("editor_toc" == "editorTOC".underscored);
-    assert("editor_toc" == "EditorTOC".underscored);
-    assert("editor42_toc" == "Editor42TOC".underscored);    
-}
+import elasticsearch.detail.string;
 
 struct NodesInfoRequest {
     enum Method = ElasticsearchMethod.get;
