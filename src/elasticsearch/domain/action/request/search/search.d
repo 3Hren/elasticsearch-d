bcdd7c45b03412fd8156e09eceb78d4ceec9dbcb
Evgeny Safronov <division494@gmail.com>
2014-03-19 00:44:28 +0400

    [API] QueryRequest now allows to specify search type.

--- src/elasticsearch/domain/action/request/search/search.d
+++ src/elasticsearch/domain/action/request/search/search.d
@@ -3,6 +3,7 @@ module elasticsearch.domain.action.request.search.search;
 import std.array;
 import std.algorithm;
 import std.conv;
+import std.string;
 
 import vibe.inet.path;
 
@@ -10,19 +11,48 @@ import elasticsearch.detail.string;
 import elasticsearch.domain.action.request.base;
 import elasticsearch.domain.action.request.method;
 
+enum SearchType {
+    DFS_QUERY_THEN_FETCH = 0,
+    QUERY_THEN_FETCH = 1,
+    DFS_QUERY_AND_FETCH = 2,
+    QUERY_AND_FETCH = 3,
+    SCAN = 4,
+    COUNT = 5,
+
+    DEFAULT = QUERY_THEN_FETCH
+}
+
 struct SearchRequest {
     enum Method = ElasticsearchMethod.GET;
     mixin UriBasedRequest!SearchRequest;
 
     private const string[] indices;
     private string[] types;
+    private SearchType searchType = SearchType.DEFAULT;
+    private string query;
 
     public this(const string[] indices...) {
         this.indices = indices.dup;
     }
 
-    public void setType(string type) {
-        this.types = [type.dup];
+    public void setType(const string type) {
+        setTypes(type);
+    }
+
+    public void setTypes(const string[] types...) {
+        this.types = types.dup;
+    }
+
+    public void setSearchType(SearchType searchType) {
+        this.searchType = searchType;
+    }
+
+    public void setQuery(const string query) {
+        this.query = query.dup;
+    }
+
+    public string data() const @property {
+        return query;
     }
 
     private void buildUri(UriBuilder builder) const {
@@ -30,6 +60,10 @@ struct SearchRequest {
             builder.setPath("_all", Strings.join(types), "_search");
         } else {
             builder.setPath(Strings.join(indices), Strings.join(types), "_search");
+        }
+
+        if (searchType != SearchType.DEFAULT) {
+            builder.addParameter("search_type", to!string(searchType).toLower());
         }
     }
 }
@@ -78,4 +112,25 @@ unittest {
     SearchRequest request = SearchRequest("twitter");
     request.setType("tweet");
     Assert.equals("/twitter/tweet/_search", request.uri);
+}
+
+unittest {
+    // Allows to specify multiple types.
+    SearchRequest request = SearchRequest("twitter");
+    request.setTypes("tweet", "compiler");
+    Assert.equals("/twitter/tweet,compiler/_search", request.uri);
+}
+
+unittest {
+    // Allows to specify search type.
+    SearchRequest request = SearchRequest("twitter");
+    request.setSearchType(SearchType.DFS_QUERY_THEN_FETCH);
+    Assert.equals("/twitter/_search?search_type=dfs_query_then_fetch", request.uri);
+}
+
+unittest {
+    // Allows to specify raw query.
+    SearchRequest request = SearchRequest();
+    request.setQuery(`{"query": {"match": {"message": "Wow!"}}}`);
+    Assert.equals(`{"query": {"match": {"message": "Wow!"}}}`, request.data);
 }
